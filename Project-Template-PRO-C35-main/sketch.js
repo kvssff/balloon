68a78aa4e954af5ea9e6d0b671c9dc5992ad0ab3
kvssff <77674259+kvssff@users.noreply.github.com>
2021-05-30 13:43:26 +0530

    Add files via upload

---
+++ Project-Template-PRO-C35-main/sketch.js
@@ -0,0 +1,67 @@
+var balloonHeight;
+var  balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database;
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+balloonHeight=database.ref("balloon/height")
+  textSize(20); 
+balloonHeight.on("value",readHeight);
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+uptadeHeight(-1,0) 
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+  uptadeHeight(1,0)
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+ uptadeHeight(0,-1);
+  }
+else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+     uptadeHeight(0,1);
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function uptadeHeight(){
+database.ref("balloon/height").set({
+x:height.x+x,
+y:height.y+y  
+})
+}
+function readHeight(data){
+height=data.val()
+balloon.x=height.x;
+balloon.y=height.y
+}
